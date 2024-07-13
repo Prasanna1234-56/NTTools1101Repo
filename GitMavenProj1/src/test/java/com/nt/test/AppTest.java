0515f1861ba46f7218b49caef21c3269e17ce970
Dell <Dell@DESKTOP-6QPPQ30>
2024-07-13 14:05:14 +0530

    TL-commit

--- GitMavenProj1/src/test/java/com/nt/test/AppTest.java
+++ GitMavenProj1/src/test/java/com/nt/test/AppTest.java
@@ -17,39 +17,19 @@ public class AppTest {
 	@Test
 	public void testWithPositives() {
 		Airthmetic ar=new Airthmetic();
-<<<<<<< HEAD
+
 		int expected=60;
 		int actual=ar.sum(40, 20);
-=======
-		int expected=40;
-		int actual=ar.sum(20, 20);
-		assertEquals(expected,actual);
-	}
-	@Test
-	public void testWithZeros() {
-		Airthmetic ar=new Airthmetic();
-		int expected=0;
-		int actual=ar.sum(0, 0);
->>>>>>> refs/heads/master
-		assertEquals(expected,actual);
-	}
-<<<<<<< HEAD
-	@Test
-	public void testWithNegatives() {
-		Airthmetic ar=new Airthmetic();
-		int expected=-40;
-		int actual=ar.sum(-20, -20);
-		assertEquals(expected,actual);
-	}
-	@Test
-	public void testWithZeros() {
-		Airthmetic ar=new Airthmetic();
-		int expected=0;
-		int actual=ar.sum(0, 0);
-		assertEquals(expected,actual);
-	}
-=======
 
->>>>>>> refs/heads/master
-	
+		assertEquals(expected,actual);
+	}
+	@Test
+	public void testWithZeros() {
+		Airthmetic ar=new Airthmetic();
+		int expected=0;
+		int actual=ar.sum(0, 0);
+
+		assertEquals(expected,actual);
+	}
+
 }
